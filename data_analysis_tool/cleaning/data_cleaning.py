144eadb8be6e7a60a8e4ac29596c5bc43af736e5
Mars-Dam <mervellousgiwa9@gmail.com>
2026-04-30 11:11:35 +0100

    data

--- data_analysis_tool/cleaning/data_cleaning.py
+++ data_analysis_tool/cleaning/data_cleaning.py
@@ -9,7 +9,36 @@ df = pd.read_csv("data_analysis_tool/data/healthcare_dataset.csv")
 # Display the first few rows of the dataset
 print(df.head())
 
+df.info()
+df.isnull().sum()
+df.duplicated().sum()
+
 df['Name'] = df['Name'].str.capitalize()
 df['Date of Admission'] = pd.to_datetime(df['Date of Admission'], errors='coerce')
 df['Discharge Date'] = pd.to_datetime(df['Discharge Date'], errors='coerce')
-print(df.head())
+
+
+
+class data_cleaning():
+    def __init__(self, df):
+        self.df = df
+
+    def clean_data(self):
+        self.df['Name'] = self.df['Name'].str.capitalize()
+        self.df['Date of Admission'] = pd.to_datetime(self.df['Date of Admission'], errors='coerce')
+        self.df['Discharge Date'] = pd.to_datetime(self.df['Discharge Date'], errors='coerce')
+        return self.df
+    
+    def save_cleaned_data(self, cleaned_df):
+        cleaned_df.to_csv("data_analysis_tool/data/cleaned_healthcare_dataset.csv", index=False)
+        print("Cleaned data saved to: data_analysis_tool/data/cleaned_healthcare_dataset.csv")
+
+cleaner = data_cleaning(df)
+cleaned_df = cleaner.clean_data()
+cleaner.save_cleaned_data(cleaned_df)
+
+
+
+
+
+
